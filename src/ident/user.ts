37e5e8b72596b9ccb61f5f8c8740561bd54a532c
Kyle Thomas <kyle@provide.services>
2020-03-04 03:33:36 -0500

    Add first and last name to user model

--- src/ident/user.ts
+++ src/ident/user.ts
@@ -3,6 +3,8 @@ import { Model } from "../model";
 export class User extends Model {
   applicationId?: string;
   name: string | undefined;
+  firstName: string | undefined;
+  lastName: string | undefined;
   email: string | undefined;
   permission: number | undefined;
   privacyPolicyAgreedAt?: string;
