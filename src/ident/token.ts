e381f6cf0edd750ef2c6d996b3911d000a3f4075
Kyle Thomas <kyle@provide.services>
2020-09-14 10:30:23 -0400

    Add access/refresh token to js package

--- src/ident/token.ts
+++ src/ident/token.ts
@@ -1,11 +1,14 @@
 import { Model } from '../model';
 
 export type Token = Model & {
+  accessToken?: string;
   applicationId?: string;
+  organizationId?: string;
   userId?: string;
   issuedAt?: string;
   expiresAt?: string;
   token?: string;
   permission?: number;
+  refreshToken?: string;
   data?: { [key: string]: any };
 };
