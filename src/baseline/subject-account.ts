ad2423ee367c06c7c5ad0c79d9abbb5e9faff346
Alex Dunne <alex12dunne@gmail.com>
2022-03-22 22:52:43 -0400

    add subject account type, update workflow and workstep types

---
+++ src/baseline/subject-account.ts
@@ -0,0 +1,29 @@
+import { Model } from '../model';
+import { Participant } from "./participant"
+
+export type SubjectAccount = Model & {
+    credentials: any
+    metadata: {
+        counterparties: Participant[]
+        networkId: string
+        organizationAddress: string
+        organizationId: string
+        organizationMessagingEndpoint: string
+        organizationProxyEndpoint: string
+        organizationRefreshToken: string
+        organizationWebsocketEndpoint: string
+        registryContractAddress: string
+        registryContract: any
+        sor: Object
+        workgroupId: string
+        vault: any
+    }
+    recoveryPolicy: any
+    role: any
+    securityPolicies: any
+    subjectId: string
+    type: string
+    
+    refreshToken: string
+    refreshTokenRaw: string
+}
