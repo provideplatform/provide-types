b66be9344b44c4e363912d947842822ac12f0bd6
Kyle Thomas <kyle@provide.services>
2022-04-08 10:41:12 -0400

    Revert "Revert "Merge pull request #16 from provideplatform/remove-model-class""
    This reverts commit 7a1e391701005cefbe198275e6dfc1cef674d025.

--- src/model.ts
+++ src/model.ts
@@ -1,80 +1,14 @@
-import { BigNumber as BigInt } from 'ethers';
+import { BigNumber as BigInt } from 'ethers'
 
-export type BigNumber = BigInt;
+export type BigNumber = BigInt
 
 export interface Error {
-  message?: string;
-  status?: number;
+  message?: string
+  status?: number
 }
 
-export class Model {
-  id?: string;
-  createdAt?: string;
-  errors?: Error[];
-
-  // marshal to JSON
-  public marshal(): string {
-    const obj = {};
-    for (const [key, value] of Object.entries(this)) {
-      if (typeof value !== 'undefined' && typeof value !== 'function') {
-        if (value instanceof Model) {
-          obj[toSnakeCase(key)] = JSON.parse(value.marshal());
-        } else {
-          obj[toSnakeCase(key)] = value;
-        }
-      }
-    }
-    return JSON.stringify(obj);
-  }
-
-  // unmarshal JSON - this is not recursive yet
-  public unmarshal(json: string): void {
-    const obj = JSON.parse(json);
-    for (const [key, value] of Object.entries(obj)) {
-      this[toCamelCase(key)] = value;
-    }
-  }
-}
-
-function isObject(o: any): boolean {
-  return o === Object(o) && !Array.isArray(o) && typeof o !== 'function';
-}
-
-function toCamelCase(str: string): string {
-  // tslint:disable-next-line: typedef
-  return str.replace(/([-_][a-z])/gi, $1 => {
-    return $1
-      .toUpperCase()
-      .replace('-', '')
-      .replace('_', '');
-  });
-}
-
-function toSnakeCase(str: string): string {
-  // tslint:disable-next-line: typedef
-  return str.replace(/[A-Z]/g, $1 => {
-    return `_${$1.toLowerCase()}`;
-  });
-}
-
-export function factory<T extends Model | Model[]>(clazz: new () => T): T {
-  return new clazz();
-}
-
-// tslint:disable-next-line: prettier
-export function unmarshal<T extends Model | Model[]>(json: string, clazz: new () => T): T | T[] {
-  const val = JSON.parse(json);
-  if (val instanceof Array) {
-    const arr = [];
-    (val as any[]).forEach((item: any) => {
-      const inst = factory(clazz);
-      (inst as Model).unmarshal(JSON.stringify(item));
-      (arr as any[]).push(inst);
-    });
-    return arr;
-  }
-
-  const instance = factory(clazz);
-  (instance as Model).unmarshal(json);
-  return instance;
+export interface Model {
+  id?: string
+  createdAt?: string
+  errors?: Error[]
 }
