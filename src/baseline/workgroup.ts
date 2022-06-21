be1a58d36a3ec0500649534f09b0d41160461a53
Alex Dunne <alex12dunne@gmail.com>
2022-06-21 18:21:55 -0400

    baseline analytics types

--- src/baseline/workgroup.ts
+++ src/baseline/workgroup.ts
@@ -14,15 +14,15 @@
  * limitations under the License.
  */
 
-import { Participant, Workflow } from '.';
-import { Model } from '../model';
+import { Participant, Workflow } from '.'
+import { Model } from '../model'
 
 // Workgroup is a baseline workgroup context
 export type Workgroup = Model & {
-	participants: Participant[];
-	workflows: Workflow[];
+	participants: Participant[]
+	workflows: Workflow[]
 
-	privacyPolicy: any;
-	securityPolicy: any;
-	tokenizationPolicy: any;
+	privacyPolicy: any
+	securityPolicy: any
+	tokenizationPolicy: any
 }
